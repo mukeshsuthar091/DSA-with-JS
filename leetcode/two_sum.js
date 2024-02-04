9b56dc04cc0401eb0329293a959e20a385945c1e
Mukesh <sutharmukesh910@gmail.com>
2024-02-04 21:35:26 +0530

    new commit

--- leetcode/two_sum.js
+++ leetcode/two_sum.js
@@ -1,3 +1,6 @@
+// This are better solution  (using hash table)
+// -----------------------------------------------------------
+
 // function twoSum(nums, target) {
     
 //     const numIndices = new Map();
@@ -37,25 +40,47 @@
 
 // ------------------------------------------------
 
+// function twoSum(nums, target){
+//     let hashTable = {};
+
+//     for(let i=0; i<nums.length; i++){
+//         if(nums[i] in hashTable){
+//             continue;
+//         }
+//         else{
+//             hashTable[nums[i]] = i;
+//         }
+//     }
+
+//     for(let i=0; i<nums.length; i++){
+//         const complement = target-nums[i];
+//         if(complement in hashTable){
+//             return [i, hashTable[complement]];
+//         }
+//     }
+//     return [];
+// }
+
+
+// This is a optimal solution (using two pointer)
+// -----------------------------------------------------------
+
 function twoSum(nums, target){
-    let hashTable = {};
+    nums.sort((a,b)=>a-b);
 
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] in hashTable){
-            continue;
+    let left = 0;
+    let right = nums.length-1;
+    while(left<right){
+        let sum = nums[left]+nums[right];
+        // console.log(sum)
+        if(sum === target){
+            return [left, right];
         }
-        else{
-            hashTable[nums[i]] = i;
-        }
+        else if(sum > target)  right--;
+        else left++;
     }
-
-    for(let i=0; i<nums.length; i++){
-        const complement = target-nums[i];
-        if(complement in hashTable){
-            return [i, hashTable[complement]];
-        }
-    }
-    return [];
 }
 
-console.log(twoSum([2,7,11,15], 9))
+
+console.log(twoSum([3,2,4], 6))
+// console.log(twoSum([2,7,11,15], 18))
